a6ebf61e9e454b4c670d1603738661ca25a219c6
PadminiDevi <Padmini.Devi.Gokarakonda@ibm.com>
2024-09-19 22:24:22 +0530

    Add files via upload

---
+++ Java/Activities/Activity1 (1).java
@@ -0,0 +1,17 @@
+package com.Activities.Activity1;
+
+
+
+public class Activity1 {
+    public static void main(String[] args) {
+    Car Honda =new Car();
+    Honda.make=2014;
+    Honda.color="Blue";
+    Honda.transmission="Automatic";
+
+    Honda.displayCharacteristics();
+    Honda.accelarate();
+    Honda.brake();
+}
+
+}
